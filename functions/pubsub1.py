951b0370b2fcc3aadcfa83be3bdfa222f06ec767
m087494 <DP2020sh>
2021-10-24 01:05:50 -0500

    Time now reporting correct year

--- functions/pubsub1.py
+++ functions/pubsub1.py
@@ -7,8 +7,8 @@ def pubsub_to_bigq(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message, file=sys.stderr)
     pubsub_message_json = json.loads(pubsub_message)
-    pubsub_message_json['Time'] = datetime.datetime.fromtimestamp(pubsub_message_json['Time'] / 1e3)
-    to_bigquery(os.environ['dataset'], os.environ['table'], pubsub_message_json)
+    pubsub_message_json['Time'] = datetime.datetime.fromtimestamp(pubsub_message_json['Time'])
+    #to_bigquery(os.environ['dataset'], os.environ['table'], pubsub_message_json)
 
 
 def to_bigquery(dataset, table, document):
